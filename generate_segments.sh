2ae20504ef936d766f19f695ac29a4e7e1f280b6
Christian Huber <christian.huber@kit.edu>
2023-08-15 18:23:10 +0200

    Ran segmentation

--- generate_segments.sh
+++ generate_segments.sh
@@ -4,5 +4,5 @@ mkdir -p segfiles
 for seg in shas_output/*.seg
 do
     seg2=${seg#shas_output/}
-    cat "$seg" | tail -n+3 | head -n-1 | cut -d" " -f3 | sed 's/:$//g' | awk -v seg="${seg2%.seg}" -F"-" '{print seg"_"NR" /project/OML/chuber/2023/data/cs231n_2017-dataset/audio/"seg".wav "$1" "$2}' > "segfiles/${seg2%.seg}.seg.aligned"
+    cat "$seg" | tail -n+3 | head -n-1 | cut -d" " -f3 | sed 's/:$//g' | awk -v seg="${seg2%.seg}" -F"-" '{print seg"_"NR" /project/OML/chuber/2023/data/cs_ca_CMU_2015/audio/"seg".wav "$1" "$2}' > "segfiles/${seg2%.seg}.seg.aligned"
 done
