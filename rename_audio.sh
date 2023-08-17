4dfd8cc0ee5da246dad8877d492c49a0e300d202
Christian Huber <christian.huber@kit.edu>
2023-08-17 11:27:14 +0200

    Removed spaces in file names

--- rename_audio.sh
+++ rename_audio.sh
@@ -2,11 +2,11 @@
 mkdir -p audio
 for f in Lecture*.opus
 do
-    f2=`echo $f | cut -d"." -f1 | sed "s/：/:/g" | cut -d":" -f1`".opus"
+    f2=`echo $f | cut -d"." -f1 | sed "s/：/:/g" | cut -d":" -f1 | sed "s/ /_/g"`".opus"
     mv "$f" audio/"$f2"
 done
 
-mv "Final Review Session - Carnegie Mellon - Computer Architecture 2015 - Onur Mutlu [5Oi7tvYE7fI].opus" "audio/Final Review.opus"
+mv "Final Review Session - Carnegie Mellon - Computer Architecture 2015 - Onur Mutlu [5Oi7tvYE7fI].opus" "audio/Final_Review.opus"
 
 rm *.opus
 rm *.m4a
